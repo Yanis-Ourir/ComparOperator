3caf6124ce01852ca5ca7f4dc52af25df00a42d8
essiens0025 <essiens0025@gmail.com>
2023-03-06 16:19:05 +0100

    test

--- front/index.php
+++ front/index.php
@@ -8,11 +8,37 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="styles.css">
     <title>ComparOpérator</title>
 </head>
-
 <body>
-
+    <?php include('navbar.php') ?>
+    <div class="accueil">
+        <img class="logo-acc" src="../assets/logo-test2.png">
+    </div>
+    <div>
+        <h1 style="text-align: center;">Compar Operator</h1>
+        <p style="text-align: center;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex eaque deleniti cum! Esse ipsa, rerum numquam at adipisci <br>qui temporibus quia nisi porro voluptate consequatur asperiores veniam veritatis totam non. sit amet consectetur, adipisicing elit. Ex eaque deleniti cu</p>
+     </div>
+        <h1>Nos Destinations :</h1>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-8 bak-col">
+                Column
+                <div class="row">
+                    <div class="col-6">
+                    </div>
+                    <div class="col-6">
+                    </div>
+                </div>
+                <div class="row">
+                </div>  
+            </div>
+            <div class="col-4">
+                Column
+            </div> 
+        </div>
+    </div>
 </body>
 
 </html>
